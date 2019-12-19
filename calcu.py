9bdb0f2ac2c884a50cbf020948e741e565343576
gyrian <58937681+gyrian@users.noreply.github.com>
2019-12-19 15:46:27 +0100

    Add files via upload

---
+++ calcu.py
@@ -0,0 +1,12 @@
+nombre_1=int(input("premierchiffre"))
+nombre_2=int(input("deuxieme chiffre"))
+operation=input("+"or"-"or"*"or"/")
+if operation=="+":
+	print(nombre_1 + nombre_2)
+elif operation=="-":
+	print(nombre_1-nombre_2)
+elif operation=="*":
+	print(nombre_1*nombre_2)
+elif operation=="/":
+	print(nombre_1/nombre_2)
+
---
+++ calcu2.py
@@ -0,0 +1,12 @@
+nombre_1=float(input("premierchiffre"))
+nombre_2=float(input("deuxieme chiffre"))
+operation=input("+"or"-"or"*"or"/")
+if operation=="+":
+	print(nombre_1 + nombre_2)
+elif operation=="-":
+	print(nombre_1-nombre_2)
+elif operation=="*":
+	print(nombre_1*nombre_2)
+elif operation=="/":
+	print(nombre_1/nombre_2)
+
